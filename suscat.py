c4b53ba13e5b4014161e2d5d64662ac9a0a7eb6a
Tani Barasch <Netanel.Barasch@mail.huji.ac.il>
2020-02-28 12:40:40 -0800

    function skeletons

---
+++ suscat.py
@@ -0,0 +1,11 @@
+#Author: Tani Barasch
+
+def suscat(df, col = [], n = 0.01):
+    """
+    Detect suspected erroneous numeric data in user chosen columns of a dataframe
+    
+    :param df: dataframe like object
+    :param col: list or array of column index's to check
+    :param n: number of percentage of values to identify as potentially erroneous
+    :return: dict, key as the column index and values is a list of row indecies.
+    """   

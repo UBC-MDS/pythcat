ac9f85f65889a90c975995bd6147aa90a3015d4e
eithar.alfatih@gmail.com <eithar.alfatih@gmail.com>
2020-02-29 12:01:06 -0800

    putting python functions in different files

---
+++ pythcat/misscat.py
@@ -0,0 +1,16 @@
+def misscat(df, threshold):
+    """
+    Drops rows or columns containing missing values if the number of the missing values exceeds a threshold
+
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+      The input dataframe
+    threshold : float
+      The threshold of the missing values ratio needed to drop rows or columns 
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        dataframe of after dropping missing values.
+    """

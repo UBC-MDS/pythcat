cccfc67cc73326b39b26385c639bccb90fd3b0ab
doraq <kumakoq@gmail.com>
2020-03-06 22:28:10 -0800

    add rounding 4 to the function

--- pythcat/topcorr.py
+++ pythcat/topcorr.py
@@ -45,7 +45,7 @@ def topcorr(df, k="all"):
     for j in range(i, n_features):
       drop_pair.append((df.columns[i], df.columns[j]))
   
-  corr_df = df.corr().abs().unstack().drop(labels = drop_pair).sort_values(ascending=False)
+  corr_df = df.corr().abs().round(4).unstack().drop(labels = drop_pair).sort_values(ascending=False)
   corr_df = pd.DataFrame(corr_df).reset_index()
   col_name = ['Feature 1', 'Feature 2', 'Absolute Correlation']
   corr_df.columns = col_name
